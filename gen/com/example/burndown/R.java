5856fe9e3071c0004388d8577cd536038cb9c17b
unknown <M9501424@E2ML08537.mindtree.com>
2014-10-09 17:04:31 -0400

    Configuring

--- gen/com/example/burndown/R.java
+++ gen/com/example/burndown/R.java
@@ -25,9 +25,10 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080002;
-        public static final int editText1=0x7f080001;
-        public static final int editText2=0x7f080000;
+        public static final int action_settings=0x7f080003;
+        public static final int login=0x7f080002;
+        public static final int password=0x7f080001;
+        public static final int username=0x7f080000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -36,11 +37,15 @@ public final class R {
         public static final int main=0x7f070000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050002;
+        public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050001;
-        public static final int password=0x7f050004;
-        public static final int username=0x7f050003;
+        public static final int burndown=0x7f050004;
+        public static final int login=0x7f050008;
+        public static final int password=0x7f050003;
+        public static final int stories=0x7f050005;
+        public static final int time=0x7f050006;
+        public static final int username=0x7f050002;
+        public static final int velocity=0x7f050007;
     }
     public static final class style {
         /** 
