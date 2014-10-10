c1ad20784a16fc766cdde82c56fc4e0353bfc89c
Abel Alvarez <abelalvarez@mindtree.com>
2014-10-10 16:03:23 -0400

    Nothing changed

--- gen/com/example/burndown/R.java
+++ gen/com/example/burndown/R.java
@@ -25,7 +25,8 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080003;
+        public static final int action_settings=0x7f080004;
+        public static final int listView1=0x7f080003;
         public static final int login=0x7f080002;
         public static final int password=0x7f080001;
         public static final int username=0x7f080000;
@@ -33,11 +34,13 @@ public final class R {
     public static final class layout {
         public static final int activity_choices=0x7f030000;
         public static final int activity_main=0x7f030001;
+        public static final int activity_project_list=0x7f030002;
     }
     public static final class menu {
         public static final int choices=0x7f070000;
         public static final int main=0x7f070001;
         public static final int parse_bugzilla=0x7f070002;
+        public static final int project_list=0x7f070003;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
