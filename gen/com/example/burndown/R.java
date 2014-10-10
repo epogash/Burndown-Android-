edeebb6a05645951e90fde5725303e665317967b
M9501420 <M9501420@E2ML06761.mindtree.com>
2014-10-10 13:42:51 -0400

    added choices menu

--- gen/com/example/burndown/R.java
+++ gen/com/example/burndown/R.java
@@ -26,24 +26,28 @@ public final class R {
     }
     public static final class id {
         public static final int action_settings=0x7f080003;
-        public static final int button1=0x7f080002;
+        public static final int login=0x7f080002;
         public static final int password=0x7f080001;
         public static final int username=0x7f080000;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int activity_choices=0x7f030000;
+        public static final int activity_main=0x7f030001;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int choices=0x7f070000;
+        public static final int main=0x7f070001;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int burndown=0x7f050004;
+        public static final int hello_world=0x7f05000a;
         public static final int login=0x7f050008;
         public static final int password=0x7f050003;
         public static final int stories=0x7f050005;
         public static final int time=0x7f050006;
+        public static final int title_activity_choices=0x7f050009;
         public static final int username=0x7f050002;
         public static final int velocity=0x7f050007;
     }
